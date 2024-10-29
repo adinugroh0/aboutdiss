d37b76c035aef0a29d7077f9de3958f2a0fea8fb
adinugroh0 <aditwist1@gmail.com>
2024-10-29 23:41:18 +0700

    rubah total

--- src/types/react-element-to-jsx-string.d.ts
+++
@@ -1,5 +0,0 @@
-// src/types/react-element-to-jsx-string.d.ts
-declare module "react-element-to-jsx-string" {
-  const reactElementToJSXString: (element: React.ReactElement) => string;
-  export default reactElementToJSXString;
-}
--- src/types/sonner.d.ts
+++
@@ -1,5 +0,0 @@
-// src/types/sonner.d.ts
-declare module "sonner" {
-  export function toast(message: string): void;
-  export const Toaster: React.FC;
-}
