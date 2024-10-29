9e346371cf4846c5638179e7be8b9aaa9fd6bdee
adinugroh0 <aditwist1@gmail.com>
2024-10-29 23:25:17 +0700

    tambah

---
+++ src/types/react-element-to-jsx-string.d.ts
@@ -0,0 +1,5 @@
+// src/types/react-element-to-jsx-string.d.ts
+declare module "react-element-to-jsx-string" {
+  const reactElementToJSXString: (element: React.ReactElement) => string;
+  export default reactElementToJSXString;
+}
