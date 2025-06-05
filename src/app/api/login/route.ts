23754d0813fd04b358095dc68eb27c014da54a67
adinugroh0 <aditwist1@gmail.com>
2025-06-05 21:54:39 +0700

    caba hilangkan notif web

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -3,7 +3,7 @@ import { supabase } from "@/lib/supabaseClient";
 
 export async function POST(req: Request) {
   try {
-    const { name, tujuan } = await req.json();
+    const { name } = await req.json();
 
     if (!name) {
       return NextResponse.json(
@@ -22,24 +22,7 @@ export async function POST(req: Request) {
       );
     }
 
-    // Kirim pesan WhatsApp via CallMeBot
-    const phone = process.env.WA_PHONE!;
-    const apikey = process.env.WA_API_KEY!;
-    const message =
-      tujuan === "lama"
-        ? `🔔 hai ${name} aku membuka Web Baru`
-        : `🔔 hai ${name} aku membuka ke Web Lama`;
-
-    const url = `https://api.callmebot.com/whatsapp.php?phone=${phone}&text=${encodeURIComponent(
-      message
-    )}&apikey=${apikey}`;
-
-    const waResponse = await fetch(url);
-    const waText = await waResponse.text();
-
-    console.log("CallMeBot response:", waText);
-
-    return NextResponse.json({ message: "Login sukses! Notifikasi dikirim." });
+    return NextResponse.json({ message: "Login sukses!" });
   } catch (e: any) {
     console.error("API Error:", e);
     return NextResponse.json({ message: "Terjadi kesalahan" }, { status: 500 });
